b8e346bc5f1b852920b76c2f016e116da3a69457
joejoeyjoseph <gdpp4352@gmail.com>
2021-11-01 10:40:36 -0600

    add t as index

--- inputs/submission_csv.py
+++ inputs/submission_csv.py
@@ -54,6 +54,8 @@ class submission_csv:
             t_col = 'time (UTC)'
         if 'Date & Time (UTC)' in df_all.columns:
             t_col = 'Date & Time (UTC)'
+        if 't' in df_all.columns:
+            t_col = 't'
 
         df = df.set_index(df_all[t_col]).sort_index()
         df.index.rename('t', inplace=True)
